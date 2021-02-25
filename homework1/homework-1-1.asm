ce78b070fca797a6e3b5e12adf01450f537a20e4
Peter Walsh <peter.t.walsh@stonybrook.edu>
2021-02-25 11:15:09 -0500

    Update homework-1-1.asm

--- homework1/homework-1-1.asm
+++ homework1/homework-1-1.asm
@@ -30,14 +30,14 @@ main:
 # you can add code to the .data section
 
 #---------------------------------------------------------------------------------------------------------------#
-#	PART_1_START: Validate the Number of Command-line Arguments and second command line arguement		#
-#														#
-#	Hello grader. This is the start of Part1 of the homework. I hope it's not too confusing			#
+#	PART_1_START: Validate the Number of Command-line Arguments and second command line arguement		
+#														
+#	Hello grader. This is the start of Part1 of the homework. I hope it's not too confusing			
 #---------------------------------------------------------------------------------------------------------------#
 
 #-------------------------------------------------------#
-# 	Check that we have only two args, if not	#
-#	jump to the wrong args message			#
+# 	Check that we have only two args, if not	
+#	jump to the wrong args message			
 #-------------------------------------------------------#
 
 check_args_length:
@@ -46,12 +46,12 @@ check_args_length:
 	bne $t0, $t1, print_wrong_arg
 	
 #-------------------------------------------------------#
-#	Validates the second arguement to see		#
-# 	whether it is a valid hex number or not		#
-#							#
-#	Store arg2 in register $s1, if $s1 is		#
-#	a valid hex number, then it will be stored	#
-#	in register $s0					#
+#	Validates the second arguement to see		
+# 	whether it is a valid hex number or not		
+#							
+#	Store arg2 in register $s1, if $s1 is		
+#	a valid hex number, then it will be stored	
+#	in register $s0					
 #-------------------------------------------------------#
 
 check_arg_2:
@@ -59,10 +59,10 @@ check_arg_2:
 	lw $s1, arg2_addr	# $s1 == arg2
 
 #-------------------------------------------------------#
-# 	Checks if the first character in the second	#
-#	arguement is valid				#
-#							#
-#	if arg2[0] != '0' then jump to print_error	#
+# 	Checks if the first character in the second	
+#	arguement is valid				
+#							
+#	if arg2[0] != '0' then jump to print_error	
 #-------------------------------------------------------#
 
 check_first_char:
@@ -71,10 +71,10 @@ check_first_char:
 	bne $t0, $t1, print_error
 
 #-------------------------------------------------------#
-#	Checks if the second character in the 2nd	#
-#	arguement is valid				#
-#							#
-#	if arg2[1] != 'x' then jump to print_error	#
+#	Checks if the second character in the 2nd	
+#	arguement is valid				
+#							
+#	if arg2[1] != 'x' then jump to print_error	
 #-------------------------------------------------------#
 
 check_second_char:
@@ -83,43 +83,43 @@ check_second_char:
 	bne $t0, $t1, print_error
 	
 #-------------------------------------------------------#
-#	Validates whether the remaining 8 chars		#
-#	are a valid hex number, and ignores any		#
-# 	additional characters				#
-#							#
-#	arg2[2-8] = (1, 2, ..., 9, A, B, ..., F)	#
+#	Validates whether the remaining 8 chars		
+#	are a valid hex number, and ignores any		
+# 	additional characters				
+#							
+#	arg2[2-8] = (1, 2, ..., 9, A, B, ..., F)	
 #-------------------------------------------------------#
-#	In the loop, I am also building the hex number	#
-#	at register $s0						#
-#							#
-#	Suppose we have: 30E9FFFC			#
-#							#
-#	Iteration 1:					#
-							#
-#	$s0 = 0000	*** Shift left 4 bits		#
-#	$s0 = 00000000					#
-#	$s0 = 00000000 OR 0011   *** logical OR with 3	#			
-#							#
-#	Iteration 2:					#
-#							#
-#	$s0 = 00000011	*** Shift left 4 bits		#
-#	$s0 = 000000110000  				#
-#	$s0 = $s0 OR 0		*** logical OR with 0	#
-#							#
-#	Iteration 3:					#
-#							#
-#	$s0 = 00110000	*** Shift left 4 bits		#
-#	$s0 = 001100000000				#
-#	$s0 = $s0 OR E		*** logical OR with #	#
-#	$s0 = 001100001110				#
+#	In the loop, I am also building the hex number	
+#	at register $s0						
+#							
+#	Suppose we have: 30E9FFFC			
+#							
+#	Iteration 1:					
+#							
+#	$s0 = 0000	*** Shift left 4 bits		
+#	$s0 = 00000000					
+#	$s0 = 00000000 OR 0011   *** logical OR with 3				
+#							
+#	Iteration 2:					
+#							
+#	$s0 = 00000011	*** Shift left 4 bits		
+#	$s0 = 000000110000  				
+#	$s0 = $s0 OR 0		*** logical OR with 0	
+#							
+#	Iteration 3:					
+#							
+#	$s0 = 00110000	*** Shift left 4 bits		
+#	$s0 = 001100000000				
+#	$s0 = $s0 OR E		*** logical OR with #	
+#	$s0 = 001100001110				
 #-------------------------------------------------------#
 	
 	and $s0, $s0, $0	# initialize $s0 == $0
 	
 
 check_remaining_chars:
-	addi $s1, $s1, 2   # increment to position 2 in string
-	li $s2, 8	   # counter variable for the loop
+	addi $s1, $s1, 2   	# increment to position 2 in string
+	li $s2, 8	   	# counter variable for the loop
 
 loop_1:	
 	beqz $s2, validate_arg1
@@ -175,7 +175,7 @@ loop_1:
 	li $t1, 'F'
 	beq $t0, $t1, get_letter
 	
-	j print_wrong_arg
+	j print_error
 	
 get_letter:
 	addi $t0, $t0, -55	# For letters, we want to take the (char - 65 + 10) to give us the binary number
@@ -192,16 +192,16 @@ next_1:
 	j loop_1
 	
 #-------------------------------------------------------#
-# 	Validates whether arg1_addr is valid as per	#
-# 	the instructions. 				#
-# 							#
-#	arg1[0] = (O, S, T, J, E, C, X, M)		#
-#	arg1.length >= 1				#
-#							#
-#	if (arg1 is valid) 				#
-#		jump to arg1_valid			#
-#	else 						#
-#		jump to error message			#
+# 	Validates whether arg1_addr is valid as per	
+# 	the instructions. 				
+# 							
+#	arg1[0] = (O, S, T, J, E, C, X, M)		
+#	arg1.length >= 1				
+#							
+#	if (arg1 is valid) 				
+#		jump to arg1_valid			
+#	else 						
+#		jump to error message			
 #-------------------------------------------------------#
 
 validate_arg1:
@@ -235,14 +235,14 @@ validate_arg1:
 	j print_error
 	
 #-------------------------------------------------------#
-#	For this, we are going to attempt to get	#
-#	the value for option 'O'			#
-#							#
-#	Step 1: Move our hex number ($s0) into $a0	#
-#							#
-#	Step 2: Shift right 26 bits (get last 6 bits)	#
-#							#
-#	Step 3: Jump to print_integer			#
+#	For this, we are going to attempt to get	
+#	the value for option 'O'			
+#							
+#	Step 1: Move our hex number ($s0) into $a0	
+#							
+#	Step 2: Shift right 26 bits (get last 6 bits)	
+#							
+#	Step 3: Jump to print_integer			
 #-------------------------------------------------------#
 	
 part_2_A:
@@ -252,104 +252,107 @@ part_2_A:
 	j print_integer		# Step 3
 	
 #-------------------------------------------------------#
-#	Getting the value for option S			#
-#							#
-#	Step 1: load Hex_7 and Hex_6			#
-#							#
-#	Step 2: Shift Hex_7 left 3 bits			#
-#							#
-#	Step 3: Shift Hex_6 right 1 bit			#
-#							#
-#	Step 4: Perform logical OR on Hex_6 and Hex_7	#
-#							#
-#	Step 5: Perform logical AND on Step 4 and 31	#
+#	Getting the value for option S	(rs nibble)	
+#							
+#	Step 1: Move $s0 (our hex number) into $a0	
+#							
+#	Step 2: shift right 21 bits 			
+#							
+#	Step 3: AND with 0x1F to isolate 5 least     	
+#		significant bits			
+#							
+#	Step 4: Print contents of $a0			
 #-------------------------------------------------------#
 
 part_2_B:
 
-	or $a0, $s0, $0
-	srl $a0, $a0, 21
-	andi $a0, $a0, 0x1F
-	j print_integer
+	or $a0, $s0, $0			# Step 1
+	
+	srl $a0, $a0, 21		# Step 2
+	
+	andi $a0, $a0, 0x1F		# Step 3
+	
+	j print_integer			# Step 4
 	
 #-------------------------------------------------------#
-#	Get the value for option T (rt nibble)		#
-#							#
-#	Step 1: Load Hex_6 and Hex_5			#
-#							#
-#	Step 2: Shift Hex_6 left 4 bits			#
-#							#
-#	Step 3: Take logical and Hex_6 and Hex_5	#
-#							#
-#	Step 4: Take logical and of result and 31	#
-#							#
-#	Step 5: Store result in $s0			#
+#	Get the value for option T (rt nibble)		
+#							
+#	Step 1: move our hex numbeer into $a0		
+#							
+#	Step 2: shift 16 bits to the right		
+#							
+#	Step 3: AND with 0x1F (isolate 5 least 		
+#		significant bits
+#							
+#	Step 4: print contents of $a0			
 #-------------------------------------------------------#
 
 part_2_C:
 
-	or $a0, $s0, $0
-	srl $a0, $a0, 16
-	andi $a0, $a0, 0x1F
+	or $a0, $s0, $0			# Step 1
 	
-	j print_integer
+	srl $a0, $a0, 16		# Step 2
+	
+	andi $a0, $a0, 0x1F		# Step 3
+	
+	j print_integer			# Step 4
 	
 #-------------------------------------------------------#
-#	Get the value for option I			#
-#							#
-# 	Step 1: Load Hex_1 - Hex_4			#
-#							#
-#	Step 2: Shift Hex_4 12 bits to left		#
-#							#
-#	Step 3: Shift Hex_3 8 bits to left		#
-#							#
-#	Step 4: Shift Hex_2 4 bits to left		#
-# 							#
-#	Step 5: OR Hex_4, Hex_3				#
-#							#
-#	Step 6: OR (5) and Hex_2			#
-#							#
-#	Step 7: OR (6) and Hex_1			#
-#							#
-#	Step 8: Check sign bit				#
-#							#
-#	if positive then print number else...		#
-#							#
-#	Step 9: xor (7) with 65535, to get complement	#	
-#							#
-#	Step 10: add one to (9) and multiply by -1	#
+#	Get the value for option I			
+#							
+# 	Step 1: move our hex number into $a0		
+#							
+#	Step 2: AND with FFFF to get 16-bit immediate	
+#							
+#	Step 3: AND with 7FFF to get sign bit ($t0)	
+#							
+#	Step 4: if ($t0 == 0): print $a0 (positive)	
+# 							
+#	Step 5: else: $a0 == negative number, so	
+#		flip all bits using XOR FFFF		
+#							
+#	Step 6: add 1 to $a0 (make it twos comp)	
+#							
+#	Step 7: multiply $a0 by negative 1		
+#							
+#	Step 8: print out $a0				
+#							
 #-------------------------------------------------------#
 
 part_2_D:
 
-	andi $a0, $s0, 0xFFFF
-	andi $t0, $a0, 0x7FFF	# we want to get the sign bit
-	beqz $t0 print_integer	# if sign-bit == 0, then print out the integer
+	andi $a0, $s0, 0xFFFF		# Step 1
 	
-	xori $a0, $a0, 0xFFFF	# flip all of the bits
-	addi $a0, $a0, 1	# add one 
+	andi $t0, $a0, 0x7FFF		# Step 2
 	
-	li $t0, -1	
-	mul $a0, $a0, $t0	# multiply by -1
+	beqz $t0 print_integer		# Step 3
 	
-	j print_integer
+	xori $a0, $a0, 0xFFFF		# Step 4
+	
+	addi $a0, $a0, 1		# Step 5
+	
+	li $t0, -1			# Step 6
+	
+	mul $a0, $a0, $t0		# Step 7
+	
+	j print_integer			# Step 8
 	
 #---------------------------------------------------------------------------------------------------------------#
-#		PART_2_END - this is the end of part 2								#
+#		PART_2_END - this is the end of part 2								
 #---------------------------------------------------------------------------------------------------------------#
 
 #---------------------------------------------------------------------------------------------------------------#
-#		PART_3_START - this is the start of part 3 - odd or even hex number				#
+#		PART_3_START - this is the start of part 3 - odd or even hex number				
 #---------------------------------------------------------------------------------------------------------------#
 
 #-------------------------------------------------------#
-#	This is going to be part 3, to check 		#
-#	whether or not the given hex nuumber		#
-#	is even or odd.					#
-#							#
-#	All I'm going to do is check if the least	#
-#	significant bit is 0 or 1, and therefore	#
-#	even or odd respectively.			#
+#	This is going to be part 3, to check 		
+#	whether or not the given hex nuumber		
+#	is even or odd.					
+#							
+#	All I'm going to do is check if the least	
+#	significant bit is 0 or 1, and therefore	
+#	even or odd respectively.			
 #-------------------------------------------------------#
 
 part_3:
@@ -363,25 +366,25 @@ is_even:
 	j print_string
 	
 #---------------------------------------------------------------------------------------------------------------#
-# 		PART_3_END - this is the end of part 3								#
+# 		PART_3_END - this is the end of part 3								
 #---------------------------------------------------------------------------------------------------------------#
 
 #---------------------------------------------------------------------------------------------------------------#
-#		PART_4_START: Counting - this is the start of part 4						#
+#		PART_4_START: Counting - this is the start of part 4						
 #---------------------------------------------------------------------------------------------------------------#
 
 #-------------------------------------------------------#
-#	For counting the number of 1's, I want		#
-#	to perform logical AND on each of the 32 bits.	#
-#							#
-#	If the result of logical AND is 0, then there 	#
-#	is a zero at that position in the hex number,	#
-#	if I get a number, then there's a one.		#
+#	For counting the number of 1's, I want		
+#	to perform logical AND on each of the 32 bits.	
+#							
+#	If the result of logical AND is 0, then there 	
+#	is a zero at that position in the hex number,	
+#	if I get a number, then there's a one.		
 #-------------------------------------------------------#
 
 part_4:
 	
-	add $a0, $0, $0		# let $a0 = number of 1's in the hex number
+	add $a0, $0, $0			# let $a0 = number of 1's in the hex number
 	addi $s1, $0, 32		# let $s1 = counter variable
 	
 loop_2:
@@ -395,15 +398,15 @@ next_2:
 	j loop_2
 
 #-----------------------------------------------------------------------------------------------------------------------#
-#		PART_4_END - this is the end of part 4									#
+#		PART_4_END - this is the end of part 4									
 #-----------------------------------------------------------------------------------------------------------------------#
 
 #-----------------------------------------------------------------------------------------------------------------------#
-#		PART_5_START: Floating Point - this is the start of part 5						#								#
+#		PART_5_START: Floating Point - this is the start of part 5														#
 #-----------------------------------------------------------------------------------------------------------------------#
 
 #-------------------------------------------------------#
-#	Part 5 A, extracting the exponent		#
+#	Part 5 A, extracting the exponent		
 #-------------------------------------------------------#
 
 part_5_A:
@@ -415,7 +418,7 @@ part_5_A:
 	j print_integer			# print out $a0
 	
 #-------------------------------------------------------#
-#	Part 5B, printing the mantissa			#
+#	Part 5B, printing the mantissa			
 #-------------------------------------------------------#
 part_5_B:
 
@@ -424,10 +427,10 @@ part_5_B:
 	syscall
 	
 #-------------------------------------------------------#
-#	This is the setup for Part 5B, I want		#
-#	to do a few things initially to make 		#
-#	the loop for printing out each bit as 		#
-#	simple as possible				#
+#	This is the setup for Part 5B, I want		
+#	to do a few things initially to make 		
+#	the loop for printing out each bit as 		
+#	simple as possible				
 #-------------------------------------------------------#
 	
 setup:
@@ -467,8 +470,8 @@ next_3:
 	j loop_3
 	
 #-------------------------------------------------------#
-# 	Prints the wrong arg message, then jumps 	#             
-# 	to exit				   		#
+# 	Prints the wrong arg message, then jumps 	         
+# 	to exit				   		
 #-------------------------------------------------------#
 
 print_wrong_arg:
@@ -476,8 +479,8 @@ print_wrong_arg:
 	j print_string
 	
 #-------------------------------------------------------#
-#	Prints the error message arg, then jump		            #
-#	to exit						                                    #
+#	Prints the error message arg, then jump		
+#	to exit						
 #-------------------------------------------------------#
 
 print_error:
@@ -485,8 +488,8 @@ print_error:
 	j print_string
 	
 #-------------------------------------------------------#
-#	Prints a string value located in $a0		#
-#	then jumps to exit				#
+#	Prints a string value located in $a0		
+#	then jumps to exit				
 #-------------------------------------------------------#
 
 print_string:
@@ -495,8 +498,8 @@ print_string:
 	j exit
 
 #-------------------------------------------------------#
-#	Prints an integer value located in $a0		#
-#	then jumps to exit				#
+#	Prints an integer value located in $a0		
+#	then jumps to exit				
 #-------------------------------------------------------#
 
 print_integer:
@@ -505,9 +508,9 @@ print_integer:
 	j exit
 
 #-------------------------------------------------------#
-#		Exits the program			#
+#		Exits the program			
 #-------------------------------------------------------#
 
 exit:
-	li $v0, 10		# exiting program
+	li $v0, 10		
 	syscall
