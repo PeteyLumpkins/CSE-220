62a09133e23149b279289b17fa226b2f8ea64a95
Peter Walsh <peter.t.walsh@stonybrook.edu>
2021-04-20 23:17:19 -0400

    Update hw4.asm

--- homework4/hw4.asm
+++ homework4/hw4.asm
@@ -267,27 +267,11 @@ add_person_property:	# $a0 -> network base addr, $a1 -> base address of person n
 	
 # First -> check property == "NAME"
 
-	addi $sp, $sp, -5				# Allocate stack space for characters -> "N", "A", "M", "E", "\0"
-	li $t0, 'N'					
-	sb $t0, 0($sp)
-	
-	li $t0, 'A'
-	sb $t0, 1($sp)
-	
-	li $t0, 'M'
-	sb $t0, 2($sp)
-	
-	li $t0, 'E'
-	sb $t0, 3($sp)
-	
-	li $t0, '\0'
-	sb $t0, 4($sp)
-	
 	move $a0, $a2					# arg1 -> property string
-	move $a1, $sp					# arg2 -> "NAME" + "\0"
-	jal str_equals					
+	move $a1, $s0					# arg2 -> "NAME" + "\0"
+	addi $a1, $a1, 24				# increment to the name string
 	
-	addi $sp, $sp, 5				# restore the stack pointer
+	jal str_equals					
 	
 	beqz $v0, add_person_property_E0		# if prop_name != "NAME" -> return error, $v0 == 0
 	
@@ -431,7 +415,8 @@ is_relation_exists_person21:
 	j is_relation_exists_loop_next			# else -> goto next edge
 	
 is_relation_does_exist:
-	li $v0, 1					# if a relation exists -> return $v0 == 1
+	li $v0, 1					# if a relation exists -> return $v0 == 1, $v1 == base addr of edge
+	move $v1, $s1
 	j is_relation_exists_done			
 	
 is_relation_doesnt_exist:
@@ -523,6 +508,7 @@ add_relation:		# $a0 -> network base addr, $a1 -> base addr of person1, $a2 -> b
 	sw $s2, 4($t0)					# edge.p2 == person2
 	sw $0, 8($t0)					# initialize friend property == 0 (just for safetly reasons)
 	
+	lw $t2, 20($s0)					# get current edges in the network
 	addi $t2, $t2, 1				# increment total edges in the network
 	sw $t2, 20($s0)					# add 1 edge to total edges in the network
 	
@@ -561,13 +547,269 @@ add_relation_done:
 	
 ###################################################################################################################################
 
-add_relation_property:
-	jr $ra
-is_friend_of_friend:
+add_relation_property:	# $a0 -> network base address, $a1 -> person1, $a2 -> person2, $a3 -> property name, $a4 -> property value
+
+# Preamble
+
+	move $fp, $sp					# save current $sp to $fp -> so we can pick up the property value later
+	
+	addi $sp, $sp, -20				# allocating stack space
+	sw $ra, 0($sp)					# $ra -> making function calls to exist functions probably
+	sw $s0, 4($sp)					# $s0 -> copy of $a0, base address of the network
+	sw $s1, 8($sp)					# $s1 -> copy of $a1, base address of person1
+	sw $s2, 12($sp)					# $s2 -> copy of $a2, base address of person2
+	sw $s3, 16($sp)					# $s3 -> copy of $a3, base address of property name
+	
+	move $s0, $a0					# save copy of $a0
+	move $s1, $a1					# save copy of $a1
+	move $s2, $a2					# save copy of $a2
+	move $s3, $a3					# save copy of $a3
+	
+# First -> check if a relation exists between person1 and person2
+
+	move $a0, $s0					# arg1 -> network base address
+	move $a1, $s1					# arg2 -> person1 base address
+	move $a2, $s2					# arg3 -> person2 base address
+	jal is_relation_exists
+	
+	beqz $v0, add_relation_property_E0		# if person1 and person2 are not related -> return error, $v0 == 0
+	
+# Next -> check if the property name == "FRIEND"
+	
+	move $a0, $s3					# arg1 -> base address of the friend property
+	move $a1, $s0					# arg2 -> "FRIEND" + '\0'
+	addi $a1, $a1, 29				# increment to the friend string
+	
+	jal str_equals					
+					
+	beqz $v0, add_relation_property_E1		# if property_value != "FRIEND" -> return error, $v0 == -1
+	
+# Next -> check if the property_value <= 0
+
+	lw $t0, 0($fp)					# load property_value from the stack
+	bltz $t0, add_relation_property_E2		# if property_value <= 0 -> return error, $v0 == -2
+	
+# Next -> all errors accounted for, so now we can add this property to the edge
+
+	move $a0, $s0					# arg1 -> network base address
+	move $a1, $s1					# arg2 -> person1 base address
+	move $a2, $s2					# arg3 -> person2 base address
+	jal get_relationship	
+	
+	# $v1 -> should be the base address of the relationship (edge) between person1 and person2
+	
+	lw $t0, 0($fp)					# load the friend value again
+	sw $t0, 8($v1)					# store friend value to third word value of an edge
+	
+# Next -> we're done so we can safely return $v0 == 1
+	
+	li $v0, 1
+	j add_relation_property_done			# if we add the friend property -> return success, $v0 == 1
+	
+add_relation_property_E0:				# if person1 isn't related to person2 -> return error, $v0 == 0
+	li $v0, 0
+	j add_relation_property_done
+
+add_relation_property_E1:				# if property_name != "FRIEND" -> return error, $v0 == -1
+	li $v0, -1
+	j add_relation_property_done
+
+add_relation_property_E2:				# if property_value < 0 -> return error, $v0 == -2
+	li $v0, -2
+	j add_relation_property_done
+	
+add_relation_property_done:
+
+# Postamble
+
+	lw $s3, 16($sp)
+	lw $s2, 12($sp)
+	lw $s1, 8($sp)
+	lw $s0, 4($sp)
+	lw $ra, 0($sp)
+	
+	addi $sp, $sp, 20
+	
+	jr $ra # $v0 = 1 -> success, $v0 = 0 -> person1 not related person2, $v0 = -1 -> prop_name != "FRIEND", $v0 = -2 -> prop_val < 0
+
+###################################################################################################################################
+	
+is_friend_of_friend:	# $a0 -> network base address, $a1 -> person1 name base address, $a2 -> person2 name base address
+
+# Preamble
+
+	addi $sp, $sp, -28
+	sw $ra, 0($sp)					# $ra -> definitly going to be calling functions in here
+	
+	sw $s0, 4($sp)					# $s0 -> copy of $a0, network base address
+	sw $s1, 8($sp)					# $s1 -> copy of $a1, person1 string base address
+	sw $s2, 12($sp)					# $s2 -> copy of $a2, person2 string base address
+	
+	# $s1 -> if person1's name is in the network, we can update $s1 to just be the address of $a1's name in the network
+	# $s2 -> if person2's name is in the network, we can update $s2 to just be the address of $a2's name in the network
+	
+	sw $s3, 16($sp)					# $s3 -> size of an edge in the network
+	sw $s4, 20($sp)					# $s4 -> total edges in the network (counter variable)
+	sw $s5, 24($sp)					# $s5 -> starting address of the first edge in the network
+	
+	move $s0, $a0					# copy of $a0 (network base address)
+	move $s1, $a1					# copy of $a1 (person1 str base address)
+	move $s2, $a2					# copy of $a2 (person2 str base address)
+	
+# First -> check if person1 exists in the network
+
+	# move $a0, $s0					# arg1 -> network base address
+	# move $a1, $s1					# arg2 -> person1 base address
+	jal is_person_name_exists			
+	
+	beqz $v0, is_friend_of_friend_error		# if person1 doesn't exist -> return error, -1
+	move $s1, $v1					# else -> person1 exists -> save address of person1 in the network for later
+	
+# Next -> check if person2 exists in the network
+	
+	move $a0, $s0
+	move $a1, $s2
+	jal is_person_name_exists
+	
+	beqz $v0, is_friend_of_friend_error		# if person2 doesn't exist -> return error, -1
+	move $s2, $v1					# else -> person2 exists -> save address of person2 in the network for later
+	
+# Next -> check if p1 == p2 -> if so return $v0 == 0
+
+	beq $s1, $s2, is_friend_of_friend_false		# if p1 == p2 -> then return $v0 == 0 -> can't be friends with yourself I guess...
+	
+# Next -> initialize loop counter variables and other loop stuff
+
+	move $a0, $s0					# arg1 -> network base address
+	jal get_first_edge	
+	
+	move $s5, $v0					# save base address of first edge to $s5
+	
+	lw $s4, 20($s0)					# get total edges in the network
+	lw $s3, 12($s0)					# get the size of an edge in the network
+	
+# Next -> alright now that the loop varaibles are setup, we can start to iterate here
+
+is_friend_of_friend_loop:
+
+	beqz $s4, is_friend_of_friend_false		# if we've seen all the edges in the network -> return false, $v0 == 0
+	
+	lw $t0, 8($s5)					# get value of "friend" on the current edge
+	blez $t0, is_friend_of_friend_next		# if (prop_value <= 0) -> go to the next edge (not a valid friendship)
+	
+	lw $t0, 0($s5)					# get edge.p1
+	beq $s1, $t0, is_friend_of_friend_edge_p2	# if p1 == edge.p1 -> check if edge.p2 and p2 have a connection
+
+	lw $t0, 4($s5)					# get edge.p2
+	beq $s1, $t0, is_friend_of_friend_edge_p1	# if p1 == edge.p2 -> check if edge.p1 and p1 have a connection
+	
+	j is_friend_of_friend_next			# if p1 is not on either edge -> then go to the next edge
+	
+is_friend_of_friend_edge_p2:
+		
+	move $a0, $s0					# arg1 -> network base address
+	move $a1, $s2					# arg2 -> person2 base address
+	lw $a2, 4($s5)					# arg3 -> edge.p2 base address
+	
+	jal is_relation_exists				# check if relation exists
+	beqz $v0, is_friend_of_friend_next			# if there is no relation between these two -> goto next edge
+	
+	lw $t0, 8($v1)					# else -> check if relationship is a friendship
+	blez $t0, is_friend_of_friend_next		# if not friends -> then go to next edge in the network
+	
+	j is_friend_of_friend_true			# else -> they are friends! Therefore p1 -> pX -> p2 is true!
+
+is_friend_of_friend_edge_p1:
+	
+	move $a0, $s0					# arg1 -> network base address
+	move $a1, $s2					# arg2 -> person2 base address
+	lw $a2, 0($s5)					# arg3 -> edge.p1 base address
+	
+	jal is_relation_exists				# check if relation exists
+	beqz $v0, is_friend_of_friend_next		# if no relation between the two -> goto the next edge
+	
+	lw $t0, 8($v1)					# else -> check if this relation is a friendship
+	blez $t0, is_friend_of_friend_next		# if relation is not a friendship
+	
+	j is_friend_of_friend_true			# else -> relation is a friendship! -> p1 -> pX -> p2 exists!
+	
+is_friend_of_friend_next:
+	
+	add $s5, $s5, $s3				# increment to the next edge in the network
+	addi $s4, $s4, -1				# decrement edges left in network
+	j is_friend_of_friend_loop
+	
+is_friend_of_friend_true:				# if we found a friend of a friend -> return true, $v0 == 1
+	li $v0, 1
+	j is_friend_of_friend_done
+
+is_friend_of_friend_false:				# if we can't find a friend of a friend -> return false, $v0 == 0
+	li $v0, 0
+	j is_friend_of_friend_done
+	
+is_friend_of_friend_error:				# if one of the names doesn't exist in the network -> return error, $v0 == -1
+	li $v0, -1
+	j is_friend_of_friend_done
+
+is_friend_of_friend_done:
+
+# Postamble
+
+	lw $s5, 24($sp)
+	lw $s4, 20($sp)
+	lw $s3, 16($sp)
+	lw $s2, 12($sp)					# restore $s registers and stack pointer
+	lw $s1, 8($sp)
+	lw $s0, 4($sp)
+	
+	lw $ra, 0($sp)
+	
+	addi $sp, $sp, 28
+
 	jr $ra
 	
 ###################################################################################################################################
 # 
 # Additional helper methods start here!!!
 #
+###################################################################################################################################
+
+get_relationship:	# $a0 -> network base address, $a1 -> person1 base address, $a2 -> person2 base address
+
+# Preamble
+
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+# Body
+
+	move $a0, $a0					# arg1 -> network base address
+	move $a1, $a1					# arg2 -> person1 base address
+	move $a2, $a2					# arg3 -> person2 base address
+	jal is_relation_exists
+	
+	# We are essentially returning exactly what is_relation_exists returns, except, well $v1 == base addr of edge officially
+
+# Postamble
+
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
+###################################################################################################################################
+
+get_first_edge:		# $a0 -> network base address
+
+# No preamble -> don't need any extra registers here
+
+	lw $t0, 8($a0)					# get the size of node in the network
+	lw $t1, 0($a0)					# get the total nodes in the network
+	
+	mul $t0, $t0, $t1				# (node_size) * (node_capacity)
+	addi $t0, $t0, 36				# increment by 36
+	add $v0, $t0, $a0				# add base address of the network to the total
+	
+# No postamble -> don't need to restore anything here (I don't think)
+
+	jr $ra		# $v0 -> base address of the first edge in the network
+	
 ###################################################################################################################################
